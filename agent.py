7db8004e2ded96ecc2306a7c7d0c09c169205610
Benedikt.Heyl <benedikt.heyl@physik.uni-muenchen.de>
2019-05-31 16:35:53 +0200

    collects whole episodes

--- agent.py
+++ agent.py
@@ -68,8 +68,9 @@ def play_episode(Game,agent_parameters):
 	rewards_batch = [[],[]]
 	game_len = 0
 	player = False
+	over = False
 
-	while game_len <= Game.size**2:
+	while game_len <= Game.size**2 and not(over):
 
 		action = policy_head(Game.board,agent_parameters[int(player)])
 		x,y = get_coords(action.item(),Game.size)
@@ -78,8 +79,6 @@ def play_episode(Game,agent_parameters):
 		states_batch[int(player)].append(Game.board)
 		actions_batch[int(player)].append(action.item())
 
-		print(Game.printField())
-
 		# checks outcome
 		res =  Game.checkboard()
 		if res:
@@ -87,16 +86,14 @@ def play_episode(Game,agent_parameters):
 			rewards_batch[int(player)] = [1.] * len(states_batch[int(player)])
 			rewards_batch[int(not(player))] = [-1.] * len(states_states_batch[int(not(player))])
 
-			Game.reset()
-
-			return states_batch,actions_batch,rewards_batch
+			over = True
 
 		if not(allowed):
 
 			rewards_batch[int(player)] = [-1.]*len(states_batch[int(player)])
 			rewards_batch[int(not(player))] = [0.]*len(states_batch[int(not(player))])
 
-			return states_batch,actions_batch,rewards_batch
+			over = True
 
 		# catches draws
 		if game_len == Game.size**2:
@@ -104,13 +101,49 @@ def play_episode(Game,agent_parameters):
 			rewards_batch[0] = [0.] * len(states_batch[0])
 			rewards_batch[1] = [0.] * len(states_batch[1])
 
-			return states_batch,actions_batch,rewards_batch
+			over = True
 
 		game_len += 1
 		player = not(player)
 
+	Game.reset()
 
 
+	return states_batch,actions_batch,rewards_batch
+
+
+def get_training_batch(Game,agent_parameters,batch_size):
+
+	collected = 0
+
+	states = [[],[]]
+	actions = [[],[]]
+	rewards = [[],[]]
+
+	while collected <= (batch_size+Game.size**2):
+		states_batch,actions_batch,rewards_batch = play_episode(Game,agent_parameters)
+
+		states[0] += states_batch[0]
+		states[1] += states_batch[1]
+
+		actions[0] += actions_batch[0]
+		actions[1] += actions_batch[1]
+
+		rewards[0] += rewards_batch[0]
+		rewards[1] += rewards_batch[1]
+
+		collected += len(rewards_batch)//2
+
+	states[0] = states[0][:batch_size]
+	states[1] = states[1][:batch_size]
+
+	actions[0] = actions[0][:batch_size]
+	actions[1] = actions[1][:batch_size]
+
+	rewards[0] = rewards[0][:batch_size]
+	rewards[1] = rewards[1][:batch_size]
+
+	return states,actions,rewards
 
 
 if __name__ == '__main__':
@@ -121,6 +154,6 @@ if __name__ == '__main__':
 	#weight initialization
 	agent_parameters = [get_weights() for x in range(2)]
 
+
 	for Epoch in range(10):
-		play_episode(Game,agent_parameters)
-		print('------------------------------------------')
+		states,actions,rewards = get_training_batch(Game,agent_parameters,batch_size)
