b6eabe31157866eacb69725c5bb87a99b163f9ea
Benedikt.Heyl <benedikt.heyl@physik.uni-muenchen.de>
2019-06-04 19:54:08 +0200

    well ittrains to do something...

--- agent.py
+++ agent.py
@@ -3,10 +3,15 @@
 
 import torch
 import tictactoe as ttt
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+
 
 board_size = 3
 n_spaces = board_size**2
-dimensions = [10,10,9]
+dimensions = [10,9]
+learning_rate = 0.05
 
 def activation(z):
 	return torch.tanh(z)
@@ -34,7 +39,7 @@ def policy_head(state,parameters):
 
 	out = infere(state,parameters)
 	exp_out = torch.exp(out)
-	return torch.argmax(exp_out/torch.sum(exp_out))
+	return exp_out/torch.sum(exp_out)
 
 
 #weight initialization
@@ -47,8 +52,8 @@ def get_weights():
 			prev_size = n_spaces*3
 		else:
 			prev_size = dimensions[i-1]
-		biases = torch.randn(size)
-		weights = torch.randn((size,prev_size))
+		biases = torch.randn(size,requires_grad=True)
+		weights = torch.randn((size,prev_size),requires_grad=True)
 
 		parameters.append([biases,weights])
 
@@ -61,7 +66,7 @@ def get_coords(n,size):
 	return x,y
 
 
-def play_episode(Game,agent_parameters):
+def play_episode(Game,agent_parameters,noise = 0.3):
 
 	states_batch = [[],[]]
 	actions_batch = [[],[]]
@@ -69,10 +74,15 @@ def play_episode(Game,agent_parameters):
 	game_len = 0
 	player = False
 	over = False
+	broke_the_rules = False
 
 	while game_len <= Game.size**2 and not(over):
 
-		action = policy_head(Game.board,agent_parameters[int(player)])
+		if random.random() > noise:
+			action = torch.argmax(policy_head(Game.board,agent_parameters[int(player)]))
+		else:
+			action = torch.tensor(random.randint(0,Game.size**2-1))
+
 		x,y = get_coords(action.item(),Game.size)
 		allowed = Game.setField(x,y)
 
@@ -84,7 +94,7 @@ def play_episode(Game,agent_parameters):
 		if res:
 
 			rewards_batch[int(player)] = [1.] * len(states_batch[int(player)])
-			rewards_batch[int(not(player))] = [-1.] * len(states_states_batch[int(not(player))])
+			rewards_batch[int(not(player))] = [-1.] * len(states_batch[int(not(player))])
 
 			over = True
 
@@ -94,6 +104,7 @@ def play_episode(Game,agent_parameters):
 			rewards_batch[int(not(player))] = [0.]*len(states_batch[int(not(player))])
 
 			over = True
+			broke_the_rules = True
 
 		# catches draws
 		if game_len == Game.size**2:
@@ -109,7 +120,7 @@ def play_episode(Game,agent_parameters):
 	Game.reset()
 
 
-	return states_batch,actions_batch,rewards_batch
+	return states_batch,actions_batch,rewards_batch,broke_the_rules
 
 
 def get_training_batch(Game,agent_parameters,batch_size):
@@ -120,8 +131,10 @@ def get_training_batch(Game,agent_parameters,batch_size):
 	actions = [[],[]]
 	rewards = [[],[]]
 
-	while collected <= (batch_size+Game.size**2):
-		states_batch,actions_batch,rewards_batch = play_episode(Game,agent_parameters)
+	btr_count= 0
+
+	while collected <= (batch_size//2+Game.size**2):
+		states_batch,actions_batch,rewards_batch,btr = play_episode(Game,agent_parameters)
 
 		states[0] += states_batch[0]
 		states[1] += states_batch[1]
@@ -134,26 +147,51 @@ def get_training_batch(Game,agent_parameters,batch_size):
 
 		collected += len(rewards_batch)//2
 
-	states[0] = states[0][:batch_size]
-	states[1] = states[1][:batch_size]
+		btr_count += int(btr)
 
-	actions[0] = actions[0][:batch_size]
-	actions[1] = actions[1][:batch_size]
+	complete_states = torch.tensor(states[0][:batch_size//2]+states[1][:batch_size//2])
+	complete_actions = torch.tensor(actions[0][:batch_size//2]+actions[1][:batch_size//2])
+	complete_rewards = torch.tensor(rewards[0][:batch_size//2]+rewards[1][:batch_size//2])
 
-	rewards[0] = rewards[0][:batch_size]
-	rewards[1] = rewards[1][:batch_size]
+	return complete_states,complete_actions,complete_rewards,float(btr_count)/(batch_size/2.)
 
-	return states,actions,rewards
+
+def loss(states,actions,rewards,parameters):
+
+	log_loss = torch.empty(states.size()[0])
+
+	for i in range(states.size()[0]):
+		log_loss[i] = rewards[i]*torch.log(policy_head(states[i],parameters)[actions[i]])
+
+	return torch.mean(log_loss)
 
 
 if __name__ == '__main__':
 
 	Game = ttt.Tictactoe(3,3)
-	batch_size = 400
+	batch_size = 2000
+	no_epochs = 500
 
 	#weight initialization
-	agent_parameters = [get_weights() for x in range(2)]
+	parameters = get_weights()
+	parameter_pair = [parameters,parameters]
+
+	reward_history = torch.empty(no_epochs)
+
+	for epoch in range(no_epochs):
+		states,actions,rewards,btr = get_training_batch(Game,parameter_pair,batch_size)
+		l = loss(states,actions,rewards,parameters)
+		reward_history[epoch] = torch.mean(rewards)
+		print('epoch: '+str(epoch)+
+		      '\tloss: '+str('{:5f}'.format(l.item()))+
+		      '\treward'+str('{:5f}'.format(torch.mean(rewards).item()))+
+		      '\tbroke the rules:'+'{:5f}'.format(btr))
+		l.backward()
+		with torch.no_grad():
+			for layer in range(len(dimensions)):
+				parameters[layer][0] -= learning_rate*parameters[layer][0].grad
+				parameters[layer][1] -= learning_rate*parameters[layer][1].grad
 
 
-	for Epoch in range(10):
-		states,actions,rewards = get_training_batch(Game,agent_parameters,batch_size)
+	plt.plot(reward_history.numpy())
+	plt.show()
